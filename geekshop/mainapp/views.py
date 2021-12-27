a2ce7e0387348bb8261e9d93eb280c14b913c127
Julio <juliojm1310@gmail.com>
2021-12-27 12:10:35 +0300

    Homework2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
-
+from datetime import datetime
 
 # Create your views here.
+mainapp_list = [{'view_name': 'index', 'link_name': 'домой'}, {'view_name': 'products', 'link_name': 'Продукты'},
+                {'view_name': 'contact', 'link_name': 'Контакты'},
+                ]
+
+
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context={
+        'now_date': datetime.now(),
+        'name': 'jarno',
+        'mainapp_list': mainapp_list,
+    })
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', context={
+        'now_date': datetime.now(),
+        'name': 'jarno',
+        'mainapp_list': mainapp_list,
+    })
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html', context={
+        'now_date': datetime.now(),
+        'name': 'jarno',
+        'mainapp_list': mainapp_list,
+        'product_types': ['все', 'дом', 'офис', 'модерн', 'классика'],
+    })
